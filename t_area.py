9c8de4389aa7e6f9f00d33297140c60e5fd27284
cr0nch <c120nch@gmail.com>
2017-08-01 23:57:38 +0100

    Week 2 - Area of a Triangle
    Calculates the area of a triangle.

---
+++ t_area.py
@@ -0,0 +1,13 @@
+# This program takes the base and height of a triangle and calculates the area.
+
+# Assign the base value of the triangle.
+base = 12
+
+# Assign the height value of the triangle.
+height = 16
+
+# Calculate the area of the triangle using the base and height.
+area = 1/2 * base * height
+
+# Print the area of the triangle.
+print(area)
